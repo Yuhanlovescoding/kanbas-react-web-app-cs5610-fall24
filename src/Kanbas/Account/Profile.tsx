b36cbff67ab8314aa569cc9cf713cbc6648c30f8
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-10-03 15:39:38 -0700

    edit account page

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -3,20 +3,34 @@ export default function Profile() {
   return (
     <div id="wd-profile-screen">
       <h3>Profile</h3>
-      <input id="wd-username" value="Yuhan Lin" placeholder="username" /><br/>
-      <input id="wd-password" value="123" placeholder="password"
-             type="password" /><br/>
-      <input id="wd-firstname" value="Yuhan" placeholder="First Name" /><br/>
-      <input id="wd-lastname" value="Lin" placeholder="Last Name" /><br/>
-      <input id="wd-dob" value="1996-02-15" type="date" /><br/>
-      <input id="wd-email" value="lin.yuhan@northeastern.edu" type="email" /><br/>
-      <select id="wd-role">
+      <input id="wd-username" value="Yuhan Lin" 
+            placeholder="username" 
+            className="form-control mb-2"/>
+      <input id="wd-password" value="123" 
+            placeholder="password"
+            type="password" 
+            className="form-control mb-2"/>
+      <input id="wd-firstname" value="Yuhan" 
+            placeholder="First Name" 
+            className="form-control mb-2"/>
+      <input id="wd-lastname" value="Lin" 
+            placeholder="Last Name"
+            className="form-control mb-2" />
+      <input id="wd-dob" value="1996-02-15" 
+            type="date" 
+            className="form-control mb-2" />
+      <input id="wd-email" value="lin.yuhan@northeastern.edu" 
+            type="email" 
+            className="form-control mb-2" />
+      <select id="wd-role" className="form-control mb-2">
         <option value="USER">User</option>
         <option value="ADMIN">Admin</option>
         <option value="FACULTY">Faculty</option>
         <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kanbas/Account/Signin" >Sign out</Link>
+      </select>
+      <Link to="/Kanbas/Account/Signin" 
+        className="btn btn-danger w-100">Sign out</Link>
+    
     </div>
   );
 }
