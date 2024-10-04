ea256bebae7089210925ff9f965dd4e3c8f991cb
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-10-03 20:58:14 -0700

    edit

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,12 +1,12 @@
-import { IoEllipsisVertical } from "react-icons/io5";  // 导入垂直省略号图标
-import { BsPlus } from "react-icons/bs";  // 导入加号图标
-import GreenCheckmark from "./GreenCheckmark";  // 复用 GreenCheckmark 组件
+import { IoEllipsisVertical } from "react-icons/io5";  
+import { BsPlus } from "react-icons/bs";  
+import GreenCheckmark from "./GreenCheckmark";  
 
 export default function ModuleControlButtons() {
   return (
     <div className="float-end">
       <GreenCheckmark />
-      <BsPlus className="fs-4 mx-2" />  {/* 加号图标，增加些间距 */}
+      <BsPlus className="fs-4 mx-2" />
       <IoEllipsisVertical className="fs-4" />
     </div>
   );
