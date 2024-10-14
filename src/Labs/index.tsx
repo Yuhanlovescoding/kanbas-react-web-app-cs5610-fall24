d641a9e6efc34f328d49b596d5a765df2c5654e5
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-10-13 17:31:29 -0700

    edit lab3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 
 export default function Labs() {
   return (
-    <div id="wd-labs">
+    <div className="p-3">
       <h1>Labs</h1>
       <h2>Yuhan Lin  |  SEC 02</h2>
       
@@ -15,7 +15,7 @@ export default function Labs() {
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
 
     </div>
