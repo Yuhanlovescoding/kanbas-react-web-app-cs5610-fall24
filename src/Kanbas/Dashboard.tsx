8bfa3850ceb14fada455bd4e495039ddfc40e669
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-11-17 00:45:05 -0800

    faculty add course

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -67,7 +67,7 @@ export default function Dashboard(
                     onChange={(e) => setCourse({ ...course, description: e.target.value })} />
           <input value={course.image || ''} className="form-control mb-2"
                  onChange={(e) => setCourse({ ...course, image: e.target.value })}
-                 placeholder="/images/reactjs.jpg" />
+                 placeholder="Enter image URL (eg./images/reactjs.jpg)" />
         </div>
       )}
 
